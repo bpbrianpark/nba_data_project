cd8d719c002ac64245e4cf602d4b935d2985c951
Brian Park <bmp1208@students.cs.ubc.ca>
2024-11-26 23:03:31 -0800

    scrapes all player links

--- backend/app/scrape/scraper.py
+++ backend/app/scrape/scraper.py
@@ -2,8 +2,36 @@ import requests
 from bs4 import BeautifulSoup
 
 def scrape_nba_data():
-    url = "https://www.nba.com/stats"
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    # Scrape and parse the data
-    return "Scraping logic here"
+    root = "https://www.basketball-reference.com"
+    website = f'{root}/leagues/NBA_2024_per_game.html'
+    print("Getting website: ", website)
+
+    response = requests.get(website)
+    content = response.text
+    soup = BeautifulSoup(content, 'lxml')
+
+    print('Classes of each table:')
+    for table in soup.find_all('table'):
+        print(table.get('class'))
+    
+    table = soup.find('table', class_='stats_table')
+    # Collecting Ddata
+    print('Classes of each table:')
+    link_list = list()
+    dupe_detector = ""
+    for row in table.tbody.find_all('tr'):
+
+        columns = row.find_all('td')
+        if (columns != []):
+            name = columns[0].text.strip()
+            if (name == dupe_detector): 
+                continue 
+            dupe_detector = name
+            hreflink = columns[0].find('a', href=True)
+            link_list.append(hreflink)
+            print(hreflink)
+    return link_list
+
+if __name__ == "__main__":
+    links = scrape_nba_data()
+    print("Scraped Links:", links.prettify())
