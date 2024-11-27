47128a3d28f6f7f33fcdbcc41f992c5860dbf14b
Brian Park <bmp1208@students.cs.ubc.ca>
2024-11-27 15:31:09 -0800

    refactoring + new tables

--- backend/app/scrape/api_helpers.py
+++ backend/app/scrape/api_helpers.py
@@ -11,18 +11,16 @@ def create_db():
         password="123",
         port="5532"
     )
-    dbname = "bbal_db_tester_00"
+    dbname = "bbal_db_tester_05"
     con.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
     cur = con.cursor()
 
-    # Create the database
     cur.execute(f"SELECT 1 FROM pg_database WHERE datname = '{dbname}'")
     exists = cur.fetchone()
     if not exists:
         cur.execute(f"CREATE DATABASE {dbname}")
     print(f"Database {dbname} is ready.")
 
-    # Connect to the newly created database
     con.close()
     con = psycopg2.connect(
         database=dbname,
@@ -33,15 +31,50 @@ def create_db():
     )
     cur = con.cursor()
 
-    # Create the table
+    # Create Per Game Table
     cur.execute("""
-        CREATE TABLE IF NOT EXISTS players2024(
+        CREATE TABLE IF NOT EXISTS pergame_2024(
+            pid INTEGER NOT NULL PRIMARY KEY,
             name VARCHAR(50) NOT NULL,
             age INTEGER NOT NULL,
             team VARCHAR(50) NOT NULL,
             pos VARCHAR(50) NOT NULL,
-            gamesPlayed INTEGER NOT NULL,
-            gamesStarted INTEGER NOT NULL,
+            games_played INTEGER NOT NULL,
+            games_started INTEGER NOT NULL,
+            minutes DECIMAL,
+            fg DECIMAL(4,2),
+            fga DECIMAL(4,2),
+            fgp DECIMAL(4,3),
+            threep DECIMAL(4,2),
+            threepa DECIMAL(4,2),
+            threepp DECIMAL(4,3),
+            twop DECIMAL(4,2),
+            twopa DECIMAL(4,2),
+            twopp DECIMAL(4,3),
+            efg DECIMAL(4,3),
+            ft DECIMAL(4,2),
+            fta DECIMAL(4,2),
+            ftp DECIMAL(4,3),
+            orb DECIMAL(4,2),
+            drb DECIMAL(4,2),
+            trb DECIMAL(4,2),
+            ast DECIMAL(4,2),
+            stl DECIMAL(4,2),
+            blk DECIMAL(4,2),
+            tov DECIMAL(4,2),
+            pf DECIMAL(4,2),
+            pts DECIMAL(4,2)
+        );
+    """)
+    # Create Per 36 Min Table
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS per36_2024(
+            name VARCHAR(50) NOT NULL,
+            age INTEGER NOT NULL,
+            team VARCHAR(50) NOT NULL,
+            pos VARCHAR(50) NOT NULL,
+            games_played INTEGER NOT NULL,
+            games_started INTEGER NOT NULL,
             minutes DECIMAL,
             fg DECIMAL(4,2),
             fga DECIMAL(4,2),
@@ -68,6 +101,171 @@ def create_db():
         );
     """)
 
+    # Create Per 100 Poss Table
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS per100_2024(
+            name VARCHAR(50) NOT NULL,
+            age INTEGER NOT NULL,
+            team VARCHAR(50) NOT NULL,
+            pos VARCHAR(50) NOT NULL,
+            games_played INTEGER NOT NULL,
+            games_started INTEGER NOT NULL,
+            minutes DECIMAL,
+            fg DECIMAL(4,2),
+            fga DECIMAL(4,2),
+            fgp DECIMAL(4,3),
+            threep DECIMAL(4,2),
+            threepa DECIMAL(4,2),
+            threepp DECIMAL(4,3),
+            twop DECIMAL(4,2),
+            twopa DECIMAL(4,2),
+            twopp DECIMAL(4,3),
+            efg DECIMAL(4,3),
+            ft DECIMAL(4,2),
+            fta DECIMAL(4,2),
+            ftp DECIMAL(4,3),
+            orb DECIMAL(4,2),
+            drb DECIMAL(4,2),
+            trb DECIMAL(4,2),
+            ast DECIMAL(4,2),
+            stl DECIMAL(4,2),
+            blk DECIMAL(4,2),
+            tov DECIMAL(4,2),
+            pf DECIMAL(4,2),
+            pts DECIMAL(4,2),
+            ortg INTEGER NOT NULL,
+            drtg INTEGER NOT NULL
+        );
+    """)
+
+    # Create Advanced Table
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS advanced_2024(
+            name VARCHAR(50) NOT NULL,
+            age INTEGER NOT NULL,
+            team VARCHAR(50) NOT NULL,
+            pos VARCHAR(50) NOT NULL,
+            games_played INTEGER NOT NULL,
+            games_started INTEGER NOT NULL,
+            minutes INTEGER NOT NULL,
+            per DECIMAL(3,1),
+            ts DECIMAL(4,3),
+            threepar DECIMAL(4,3),
+            ftr DECIMAL(4,3),
+            orbp DECIMAL(3,1),
+            drbp DECIMAL(3,1),
+            trbp DECIMAL(3,1),
+            astp DECIMAL(3,1),
+            stlp DECIMAL(3,1),
+            blkp DECIMAL(3,1),
+            tovp DECIMAL(3,1),
+            usg DECIMAL(3,1),
+            ows DECIMAL(3,1),
+            dws DECIMAL(3,1),
+            ws DECIMAL(3,1),
+            wsfourtyeight DECIMAL(4,3),
+            obpm DECIMAL(3,1),
+            dbpm DECIMAL(3,1),
+            bpm DECIMAL(3,1),
+            vorp DECIMAL(3,1)
+        );
+    """)
+
+    # Create Play-by_Play Table
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS pbp_2024(
+            name VARCHAR(50) NOT NULL,
+            age INTEGER NOT NULL,
+            team VARCHAR(50) NOT NULL,
+            pos VARCHAR(50) NOT NULL,
+            games_played INTEGER NOT NULL,
+            games_started INTEGER NOT NULL,
+            minutes INTEGER NOT NULL,
+            positionp_pg INTEGER,
+            positionp_sg INTEGER,
+            positionp_sf INTEGER,
+            positionp_pf INTEGER,
+            positionp_c INTEGER,
+            oncourtper100 DECIMAL(3,1),
+            onoffper100 DECIMAL(3,1),
+            badpassto INTEGER,
+            lostballto INTEGER,
+            shootfoul_commit INTEGER,
+            offensivefoul_commit INTEGER,
+            shootfoul_drawn INTEGER,
+            offensivefoul_drawn INTEGER,
+            pga INTEGER,
+            and1 INTEGER,
+            blkd INTEGER
+        );
+    """)
+
+    # Create Shooting Table
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS shooting_2024(
+            name VARCHAR(50) NOT NULL,
+            age INTEGER NOT NULL,
+            team VARCHAR(50) NOT NULL,
+            pos VARCHAR(50) NOT NULL,
+            games_played INTEGER NOT NULL,
+            games_started INTEGER NOT NULL,
+            minutes INTEGER NOT NULL,
+            fgp DECIMAL(4,3),
+            avg_fg_dist DECIMAL(3,1),
+            twop_shotdiet DECIMAL(4,3),
+            zero_three_shotdiet DECIMAL(4,3),
+            three_ten_shotdiet DECIMAL(4,3),
+            ten_sixteen_shotdiet DECIMAL(4,3),
+            sixteen_threep_shotdiet DECIMAL(4,3),
+            threep_shotdiet DECIMAL(4,3),
+            twop_p DECIMAL(4,3),
+            zero_three_p DECIMAL(4,3),
+            three_ten_p DECIMAL(4,3),
+            ten_sixteen_p DECIMAL(4,3),
+            sixteen_threep_p DECIMAL(4,3),
+            threep_p DECIMAL(4,3),
+            twop_assisted_p DECIMAL(4,3),
+            threep_assisted_p DECIMAL(4,3),
+            dunk_shotdiet DECIMAL(4,3),
+            dunks INTEGER,
+            cornerthreep_diet DECIMAL(4,3),
+            cornerthreep_p DECIMAL(4,3),
+            heave_attempt INTEGER,
+            heave INTEGER
+        );
+    """)
+
+    # Create Adjusted Shooting Table
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS adj_shooting_2024(
+            name VARCHAR(50) NOT NULL,
+            age INTEGER NOT NULL,
+            team VARCHAR(50) NOT NULL,
+            pos VARCHAR(50) NOT NULL,
+            games_played INTEGER NOT NULL,
+            games_started INTEGER NOT NULL,
+            minutes INTEGER NOT NULL,
+            fgp DECIMAL(4,3),
+            twopp DECIMAL(4,3),
+            threepp DECIMAL(4,3),
+            efg DECIMAL(4,3),
+            ftp DECIMAL(4,3),
+            ts DECIMAL(4,3),
+            ftr DECIMAL(4,3),
+            threepar DECIMAL(4,3),
+            fg_adj INTEGER,
+            twop_adj INTEGER,
+            threep_adj INTEGER,
+            efg_adj INTEGER,
+            ft_adj INTEGER,
+            ts_adj INTEGER,
+            ftr_adj INTEGER,
+            threepar_adj INTEGER,
+            fg_added DECIMAL(4,1),
+            ts_added  DECIMAL(4,1)
+        );
+    """)
+
     # Verify tables
     cur.execute("""
         SELECT table_name
@@ -80,10 +278,21 @@ def create_db():
     cur.close()
     con.close()
 
+def safe_get(column):
+    return column.text.strip() if column.text.strip() else None
+
+def standardize_name(raw_name):
+    name = raw_name.encode("latin1").decode("utf-8")
+    return name
+
+def generate_pid(name):
+    pid = abs(hash(name)) % (2**31)
+    return pid
+
 def add_to_db(columns):
     con = None
     con=psycopg2.connect(
-        database="bbal_db_tester_00",
+        database="bbal_db_tester_05",
         user="brianpark",
         host="localhost",
         password="123",
@@ -92,18 +301,15 @@ def add_to_db(columns):
     )
     con.commit()
     cur = con.cursor()
-
-    def safe_get(column):
-        return column.text.strip() if column.text.strip() else None
-    
     raw_name = safe_get(columns[0])
-    name = raw_name.encode("latin1").decode("utf-8")
-    age = int(safe_get(columns[1]) or 0)  # Default age to 0 if missing
+    name = standardize_name(raw_name)
+    pid = generate_pid(name)
+    age = int(safe_get(columns[1]) or 0)
     team = safe_get(columns[2])
     pos = safe_get(columns[3])
-    g = int(safe_get(columns[4]) or 0)  # Default games played to 0
-    gs = int(safe_get(columns[5]) or 0)  # Default games started to 0
-    mp = float(safe_get(columns[6]) or 0)  # Default minutes to 0
+    g = int(safe_get(columns[4]) or 0) 
+    gs = int(safe_get(columns[5]) or 0)  
+    mp = float(safe_get(columns[6]) or 0) 
     fg = float(safe_get(columns[7]) or 0)
     fga = float(safe_get(columns[8]) or 0)
     fgp = float(safe_get(columns[9]) or 0)
@@ -127,12 +333,11 @@ def add_to_db(columns):
     pf = float(safe_get(columns[27]) or 0)
     pts = float(safe_get(columns[28]) or 0)
     cur.execute(
-    "INSERT INTO players2024 (name, age, team, pos, gamesPlayed, gamesStarted, minutes, fg, fga, fgp, threep, threepa, threepp, twop, twopa, twopp, efg, ft, fta, ftp, orb, drb, trb, ast, stl, blk, tov, pf, pts) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
-    (name, age, team, pos, g, gs, mp, fg, fga, fgp, threep, threepa, threepp, twop, twopa, twopp, efg, ft, fta, ftp, orb, drb, trb, ast, stl, blk, tov, pf, pts)
+    "INSERT INTO pergame_2024 (pid, name, age, team, pos, games_played, games_started, minutes, fg, fga, fgp, threep, threepa, threepp, twop, twopa, twopp, efg, ft, fta, ftp, orb, drb, trb, ast, stl, blk, tov, pf, pts) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
+    (pid, name, age, team, pos, g, gs, mp, fg, fga, fgp, threep, threepa, threepp, twop, twopa, twopp, efg, ft, fta, ftp, orb, drb, trb, ast, stl, blk, tov, pf, pts)
     )
     con.commit()
     cur.close()
-    con.commit()
     con.close()
     
 # don't forget to commit to make changes persistent 
--- backend/app/scrape/scraper.py
+++ backend/app/scrape/scraper.py
@@ -2,24 +2,41 @@ import requests
 from bs4 import BeautifulSoup
 from api_helpers import add_to_db, create_db
 
-def scrape_nba_data():
-    root = "https://www.basketball-reference.com"
-    website = f'{root}/leagues/NBA_2024_per_game.html'
-
-    response = requests.get(website)
-    content = response.text
-    soup = BeautifulSoup(content, 'lxml')
-
-    print('Classes of each table:')
-    for table in soup.find_all('table'):
-        print(table.get('class'))
+def is_dupe(prev, curr):
+    if (prev == curr):
+        return True
+    else:
+        return False
     
-    table = soup.find('table', class_='stats_table')
-    link_list = list()
-    dupe_detector = ""
-    create_db()
-    for row in table.tbody.find_all('tr'):
+def determine_table(name, columns):
+    if (name == "Per Game"):
+        add_to_db(columns)
+        return ""
+    elif (name == "Per 36 Min"):
+        # add_to_per36_table(columns)
+        return ""
+    elif (name == "Per 100 Poss"):
+        # add_to_per100_table(columns)
+        return ""
+    elif (name == "Advanced"):
+        # add_to_advanced_table(columns)
+        return ""
+    elif (name == "Play-by-Play"):
+        # add_to_pbp_table(columns)
+        return ""
+    elif (name == "Shooting"):
+        # add_to_shooting_table(columns)
+        return ""
+    elif (name == "Adjusted Shooting"):
+        # add_to_adjshooting_table(columns)
+        return ""
+    return ""
 
+def scrape_page(content):
+    soup = BeautifulSoup(content, 'lxml')
+    table = soup.find('table', class_='stats_table')
+    dupe_detector = ""
+    for row in table.tbody.find_all('tr'):
         columns = row.find_all('td')
         if (columns != []):
             name = columns[0].text.strip()
@@ -29,10 +46,39 @@ def scrape_nba_data():
             if (name ==  lgAvg):
                 continue
             dupe_detector = name
-            hreflink = columns[0].find('a', href=True)
             add_to_db(columns)
-            # link_list.append(hreflink)
-            # print(hreflink)
+    return ""
+
+def scrape_nba_data():
+    root = "https://www.basketball-reference.com"
+    website = f'{root}/leagues/NBA_2024_per_game.html'
+
+    response = requests.get(website)
+    content = response.text
+    soup = BeautifulSoup(content, 'lxml')
+
+    sections = soup.find_all('div', class_="filter")
+    for link in sections[0].find_all('a', href=True):
+        print("Href: ", link)
+        result = requests.get(f'{root}/{link}')
+        content = result.text
+        # scrape_page(content)
+        # soup = BeautifulSoup(content, 'lxml')
+    
+    table = soup.find('table', class_='stats_table')
+    dupe_detector = ""
+    create_db()
+    for row in table.tbody.find_all('tr'):
+        columns = row.find_all('td')
+        if (columns != []):
+            name = columns[0].text.strip()
+            lgAvg = "League Average"
+            if (name == dupe_detector): 
+                continue 
+            if (name ==  lgAvg):
+                continue
+            dupe_detector = name
+            add_to_db(columns)
     return ""
 
 if __name__ == "__main__":
