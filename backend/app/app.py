7bde929799edea43869adf11bbddfb8ed15e4889
Brian Park <bmp1208@students.cs.ubc.ca>
2024-11-28 15:35:01 -0800

    deleted unncessary files

--- backend/app/app.py
+++ backend/app/app.py
@@ -7,17 +7,11 @@ api = Api()
 
 def create_app():
     app = Flask(__name__)
-
-    # Configurations
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///nba.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-    # Initialize extensions
     db.init_app(app)
     api.init_app(app)
 
-    # Register routes
-    from app.routes import routes  # Import your Blueprint
+    from app.routes import routes  
     app.register_blueprint(routes)
-
-    return app  # Ensure the app is returned here
+    return app  
