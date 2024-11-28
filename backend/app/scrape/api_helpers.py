2cf692fdc84efa5b6610729517ccc6175117b7bc
Brian Park <bmp1208@students.cs.ubc.ca>
2024-11-27 16:51:29 -0800

    refactoring + added all statistics for a given year

--- backend/app/scrape/api_helpers.py
+++ backend/app/scrape/api_helpers.py
@@ -2,15 +2,18 @@ import psycopg2
 from psycopg2 import connect
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
-DB_NAME = "bbal_db_tester_07"
+DB_NAME = "db_tester_005"
+USER = "brianpark"
+PASSWORD = "123"
+PORT = "5532"
 
 def create_db():
     con = psycopg2.connect(
         database="postgres",
-        user="brianpark",
+        user=USER,
         host="localhost",
-        password="123",
-        port="5532"
+        password=PASSWORD,
+        port=PORT
     )
     dbname = DB_NAME
     con.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
@@ -25,10 +28,10 @@ def create_db():
     con.close()
     con = psycopg2.connect(
         database=dbname,
-        user="brianpark",
+        user=USER,
         host="localhost",
         password="123",
-        port="5532"
+        port=PORT
     )
     cur = con.cursor()
 
@@ -70,6 +73,7 @@ def create_db():
     # Create Per 36 Min Table
     cur.execute("""
         CREATE TABLE IF NOT EXISTS per36_2024(
+            pid INTEGER NOT NULL PRIMARY KEY,
             name VARCHAR(50) NOT NULL,
             age INTEGER NOT NULL,
             team VARCHAR(50) NOT NULL,
@@ -105,6 +109,7 @@ def create_db():
     # Create Per 100 Poss Table
     cur.execute("""
         CREATE TABLE IF NOT EXISTS per100_2024(
+            pid INTEGER NOT NULL PRIMARY KEY,
             name VARCHAR(50) NOT NULL,
             age INTEGER NOT NULL,
             team VARCHAR(50) NOT NULL,
@@ -142,6 +147,7 @@ def create_db():
     # Create Advanced Table
     cur.execute("""
         CREATE TABLE IF NOT EXISTS advanced_2024(
+            pid INTEGER NOT NULL PRIMARY KEY,
             name VARCHAR(50) NOT NULL,
             age INTEGER NOT NULL,
             team VARCHAR(50) NOT NULL,
@@ -149,32 +155,33 @@ def create_db():
             games_played INTEGER NOT NULL,
             games_started INTEGER NOT NULL,
             minutes INTEGER NOT NULL,
-            per DECIMAL(3,1),
+            per DECIMAL(5,1),
             ts DECIMAL(4,3),
             threepar DECIMAL(4,3),
             ftr DECIMAL(4,3),
-            orbp DECIMAL(3,1),
-            drbp DECIMAL(3,1),
-            trbp DECIMAL(3,1),
-            astp DECIMAL(3,1),
-            stlp DECIMAL(3,1),
-            blkp DECIMAL(3,1),
-            tovp DECIMAL(3,1),
-            usg DECIMAL(3,1),
-            ows DECIMAL(3,1),
-            dws DECIMAL(3,1),
-            ws DECIMAL(3,1),
+            orbp DECIMAL(5,1),
+            drbp DECIMAL(5,1),
+            trbp DECIMAL(5,1),
+            astp DECIMAL(5,1),
+            stlp DECIMAL(5,1),
+            blkp DECIMAL(5,1),
+            tovp DECIMAL(5,1),
+            usg DECIMAL(5,1),
+            ows DECIMAL(5,1),
+            dws DECIMAL(5,1),
+            ws DECIMAL(5,1),
             wsfourtyeight DECIMAL(4,3),
-            obpm DECIMAL(3,1),
-            dbpm DECIMAL(3,1),
-            bpm DECIMAL(3,1),
-            vorp DECIMAL(3,1)
+            obpm DECIMAL(5,1),
+            dbpm DECIMAL(5,1),
+            bpm DECIMAL(5,1),
+            vorp DECIMAL(5,1)
         );
     """)
 
     # Create Play-by_Play Table
     cur.execute("""
         CREATE TABLE IF NOT EXISTS pbp_2024(
+            pid INTEGER NOT NULL PRIMARY KEY,
             name VARCHAR(50) NOT NULL,
             age INTEGER NOT NULL,
             team VARCHAR(50) NOT NULL,
@@ -187,8 +194,8 @@ def create_db():
             positionp_sf INTEGER,
             positionp_pf INTEGER,
             positionp_c INTEGER,
-            oncourtper100 DECIMAL(3,1),
-            onoffper100 DECIMAL(3,1),
+            oncourtper100 DECIMAL(5,1),
+            onoffper100 DECIMAL(5,1),
             badpassto INTEGER,
             lostballto INTEGER,
             shootfoul_commit INTEGER,
@@ -204,6 +211,7 @@ def create_db():
     # Create Shooting Table
     cur.execute("""
         CREATE TABLE IF NOT EXISTS shooting_2024(
+            pid INTEGER NOT NULL PRIMARY KEY,
             name VARCHAR(50) NOT NULL,
             age INTEGER NOT NULL,
             team VARCHAR(50) NOT NULL,
@@ -212,7 +220,7 @@ def create_db():
             games_started INTEGER NOT NULL,
             minutes INTEGER NOT NULL,
             fgp DECIMAL(4,3),
-            avg_fg_dist DECIMAL(3,1),
+            avg_fg_dist DECIMAL(5,1),
             twop_shotdiet DECIMAL(4,3),
             zero_three_shotdiet DECIMAL(4,3),
             three_ten_shotdiet DECIMAL(4,3),
@@ -239,6 +247,7 @@ def create_db():
     # Create Adjusted Shooting Table
     cur.execute("""
         CREATE TABLE IF NOT EXISTS adj_shooting_2024(
+            pid INTEGER NOT NULL PRIMARY KEY,
             name VARCHAR(50) NOT NULL,
             age INTEGER NOT NULL,
             team VARCHAR(50) NOT NULL,
@@ -263,7 +272,7 @@ def create_db():
             ftr_adj INTEGER,
             threepar_adj INTEGER,
             fg_added DECIMAL(4,1),
-            ts_added  DECIMAL(4,1)
+            ts_added DECIMAL(4,1)
         );
     """)
 
@@ -290,16 +299,20 @@ def generate_pid(name):
     pid = abs(hash(name)) % (2**31)
     return pid
 
-def add_to_db(columns):
-    con = None
+def connect_to_db(con):
     con=psycopg2.connect(
         database=DB_NAME,
-        user="brianpark",
+        user=USER,
         host="localhost",
-        password="123",
-        port="5532",
+        password=PASSWORD,
+        port=PORT,
         options="-c client_encoding=utf8"
     )
+    return con
+
+def add_to_pergame(columns):
+    con = None
+    con = connect_to_db(con)
     con.commit()
     cur = con.cursor()
     raw_name = safe_get(columns[0])
@@ -336,6 +349,260 @@ def add_to_db(columns):
     cur.execute(
     "INSERT INTO pergame_2024 (pid, name, age, team, pos, games_played, games_started, minutes, fg, fga, fgp, threep, threepa, threepp, twop, twopa, twopp, efg, ft, fta, ftp, orb, drb, trb, ast, stl, blk, tov, pf, pts) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
     (pid, name, age, team, pos, g, gs, mp, fg, fga, fgp, threep, threepa, threepp, twop, twopa, twopp, efg, ft, fta, ftp, orb, drb, trb, ast, stl, blk, tov, pf, pts)
+    )
+    con.commit()
+    cur.close()
+    con.close()
+
+def add_to_per36(columns):
+    con = None
+    con = connect_to_db(con)
+    con.commit()
+    cur = con.cursor()
+    raw_name = safe_get(columns[0])
+    name = standardize_name(raw_name)
+    pid = generate_pid(name)
+    age = int(safe_get(columns[1]) or 0)
+    team = safe_get(columns[2])
+    pos = safe_get(columns[3])
+    g = int(safe_get(columns[4]) or 0) 
+    gs = int(safe_get(columns[5]) or 0)  
+    mp = float(safe_get(columns[6]) or 0) 
+    fg = float(safe_get(columns[7]) or 0)
+    fga = float(safe_get(columns[8]) or 0)
+    fgp = float(safe_get(columns[9]) or 0)
+    threep = float(safe_get(columns[10]) or 0)
+    threepa = float(safe_get(columns[11]) or 0)
+    threepp = float(safe_get(columns[12]) or 0)
+    twop = float(safe_get(columns[13]) or 0)
+    twopa = float(safe_get(columns[14]) or 0)
+    twopp = float(safe_get(columns[15]) or 0)
+    efg = float(safe_get(columns[16]) or 0)
+    ft = float(safe_get(columns[17]) or 0)
+    fta = float(safe_get(columns[18]) or 0)
+    ftp = float(safe_get(columns[19]) or 0)
+    orb = float(safe_get(columns[20]) or 0)
+    drb = float(safe_get(columns[21]) or 0)
+    trb = float(safe_get(columns[22]) or 0)
+    ast = float(safe_get(columns[23]) or 0)
+    stl = float(safe_get(columns[24]) or 0)
+    blk = float(safe_get(columns[25]) or 0)
+    tov = float(safe_get(columns[26]) or 0)
+    pf = float(safe_get(columns[27]) or 0)
+    pts = float(safe_get(columns[28]) or 0)
+    cur.execute(
+    "INSERT INTO per36_2024 (pid, name, age, team, pos, games_played, games_started, minutes, fg, fga, fgp, threep, threepa, threepp, twop, twopa, twopp, efg, ft, fta, ftp, orb, drb, trb, ast, stl, blk, tov, pf, pts) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
+    (pid, name, age, team, pos, g, gs, mp, fg, fga, fgp, threep, threepa, threepp, twop, twopa, twopp, efg, ft, fta, ftp, orb, drb, trb, ast, stl, blk, tov, pf, pts)
+    )
+    con.commit()
+    cur.close()
+    con.close()
+
+def add_to_per100(columns):
+    con = None
+    con = connect_to_db(con)
+    con.commit()
+    cur = con.cursor()
+    raw_name = safe_get(columns[0])
+    name = standardize_name(raw_name)
+    pid = generate_pid(name)
+    age = int(safe_get(columns[1]) or 0)
+    team = safe_get(columns[2])
+    pos = safe_get(columns[3])
+    g = int(safe_get(columns[4]) or 0) 
+    gs = int(safe_get(columns[5]) or 0)  
+    mp = float(safe_get(columns[6]) or 0) 
+    fg = float(safe_get(columns[7]) or 0)
+    fga = float(safe_get(columns[8]) or 0)
+    fgp = float(safe_get(columns[9]) or 0)
+    threep = float(safe_get(columns[10]) or 0)
+    threepa = float(safe_get(columns[11]) or 0)
+    threepp = float(safe_get(columns[12]) or 0)
+    twop = float(safe_get(columns[13]) or 0)
+    twopa = float(safe_get(columns[14]) or 0)
+    twopp = float(safe_get(columns[15]) or 0)
+    efg = float(safe_get(columns[16]) or 0)
+    ft = float(safe_get(columns[17]) or 0)
+    fta = float(safe_get(columns[18]) or 0)
+    ftp = float(safe_get(columns[19]) or 0)
+    orb = float(safe_get(columns[20]) or 0)
+    drb = float(safe_get(columns[21]) or 0)
+    trb = float(safe_get(columns[22]) or 0)
+    ast = float(safe_get(columns[23]) or 0)
+    stl = float(safe_get(columns[24]) or 0)
+    blk = float(safe_get(columns[25]) or 0)
+    tov = float(safe_get(columns[26]) or 0)
+    pf = float(safe_get(columns[27]) or 0)
+    pts = float(safe_get(columns[28]) or 0)
+    ortg = int(safe_get(columns[29]) or 0)
+    drtg = int (safe_get(columns[30]) or 0)
+    cur.execute(
+    "INSERT INTO per100_2024 (pid, name, age, team, pos, games_played, games_started, minutes, fg, fga, fgp, threep, threepa, threepp, twop, twopa, twopp, efg, ft, fta, ftp, orb, drb, trb, ast, stl, blk, tov, pf, pts, ortg, drtg) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
+    (pid, name, age, team, pos, g, gs, mp, fg, fga, fgp, threep, threepa, threepp, twop, twopa, twopp, efg, ft, fta, ftp, orb, drb, trb, ast, stl, blk, tov, pf, pts, ortg, drtg)
+    )
+    con.commit()
+    cur.close()
+    con.close()
+
+def add_to_advanced(columns):
+    con = None
+    con = connect_to_db(con)
+    con.commit()
+    cur = con.cursor()
+    raw_name = safe_get(columns[0])
+    name = standardize_name(raw_name)
+    pid = generate_pid(name)
+    age = int(safe_get(columns[1]) or 0)
+    team = safe_get(columns[2])
+    pos = safe_get(columns[3])
+    g = int(safe_get(columns[4]) or 0) 
+    gs = int(safe_get(columns[5]) or 0)  
+    mp = float(safe_get(columns[6]) or 0) 
+    per = float(safe_get(columns[7]) or 0)
+    ts = float(safe_get(columns[8]) or 0)
+    threepar = float(safe_get(columns[9]) or 0)
+    ftr = float(safe_get(columns[10]) or 0)
+    orbp = float(safe_get(columns[11]) or 0)
+    drbp = float(safe_get(columns[12]) or 0)
+    trbp = float(safe_get(columns[13]) or 0)
+    astp = float(safe_get(columns[14]) or 0)
+    stlp = float(safe_get(columns[15]) or 0)
+    blkp = float(safe_get(columns[16]) or 0)
+    tovp = float(safe_get(columns[17]) or 0)
+    usg = float(safe_get(columns[18]) or 0)
+    ows = float(safe_get(columns[19]) or 0)
+    dws = float(safe_get(columns[20]) or 0)
+    ws = float(safe_get(columns[21]) or 0)
+    wsfourtyeight = float(safe_get(columns[22]) or 0)
+    obpm = float(safe_get(columns[23]) or 0)
+    dbpm = float(safe_get(columns[24]) or 0)
+    bpm = float(safe_get(columns[25]) or 0)
+    vorp = float(safe_get(columns[26]) or 0)
+    cur.execute(
+    "INSERT INTO advanced_2024 (pid, name, age, team, pos, games_played, games_started, minutes, per, ts, threepar, ftr, orbp, drbp, trbp, astp, stlp, blkp, tovp, usg, ows, dws, ws, wsfourtyeight, obpm, dbpm, bpm, vorp) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
+    (pid, name, age, team, pos, g, gs, mp, per, ts, threepar, ftr, orbp, drbp, trbp, astp, stlp, blkp, tovp, usg, ows, dws, ws, wsfourtyeight, obpm, dbpm, bpm, vorp)
+    )
+    con.commit()
+    cur.close()
+    con.close()
+
+def add_to_pbp(columns):
+    con = None
+    con = connect_to_db(con)
+    con.commit()
+    cur = con.cursor()
+    raw_name = safe_get(columns[0])
+    name = standardize_name(raw_name)
+    pid = generate_pid(name)
+    age = int(safe_get(columns[1]) or 0)
+    team = safe_get(columns[2])
+    pos = safe_get(columns[3])
+    g = int(safe_get(columns[4]) or 0) 
+    gs = int(safe_get(columns[5]) or 0)  
+    mp = float(safe_get(columns[6]) or 0) 
+    positionp_pg = int(safe_get(columns[7]) or 0)
+    positionp_sg = int(safe_get(columns[8]) or 0)
+    positionp_sf = int(safe_get(columns[9]) or 0)
+    positionp_pf = int(safe_get(columns[10]) or 0)
+    positionp_c =  int(safe_get(columns[11]) or 0)
+    oncourtper100 = float(safe_get(columns[12]) or 0)
+    onoffper100 = float(safe_get(columns[13]) or 0)
+    badpassto = int(safe_get(columns[14]) or 0)
+    lostballto = int(safe_get(columns[15]) or 0)
+    shootfoul_commit = int(safe_get(columns[16]) or 0)
+    offensivefoul_commit = int(safe_get(columns[17]) or 0)
+    shootfoul_drawn = int(safe_get(columns[18]) or 0)
+    offensivefoul_drawn = int(safe_get(columns[19]) or 0)
+    pga = int(safe_get(columns[20]) or 0)
+    and1 = int(safe_get(columns[21]) or 0)
+    blkd = int(safe_get(columns[22]) or 0)
+    cur.execute(
+    "INSERT INTO pbp_2024 (pid, name, age, team, pos, games_played, games_started, minutes, positionp_pg, positionp_sg, positionp_sf, positionp_pf, positionp_c, oncourtper100, onoffper100, badpassto, lostballto, shootfoul_commit, offensivefoul_commit, shootfoul_drawn, offensivefoul_drawn, pga, and1, blkd) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
+    (pid, name, age, team, pos, g, gs, mp, positionp_pg, positionp_sg, positionp_sf, positionp_pf, positionp_c, oncourtper100, onoffper100, badpassto, lostballto, shootfoul_commit, offensivefoul_commit, shootfoul_drawn, offensivefoul_drawn, pga, and1, blkd)
+    )
+    con.commit()
+    cur.close()
+    con.close()
+
+def add_to_shooting(columns):
+    con = None
+    con = connect_to_db(con)
+    con.commit()
+    cur = con.cursor()
+    raw_name = safe_get(columns[0])
+    name = standardize_name(raw_name)
+    pid = generate_pid(name)
+    age = int(safe_get(columns[1]) or 0)
+    team = safe_get(columns[2])
+    pos = safe_get(columns[3])
+    g = int(safe_get(columns[4]) or 0) 
+    gs = int(safe_get(columns[5]) or 0)  
+    mp = float(safe_get(columns[6]) or 0) 
+    fgp = float(safe_get(columns[7]) or 0)
+    avg_fg_dist = float(safe_get(columns[8]) or 0)
+    twop_shotdiet = float(safe_get(columns[9]) or 0)
+    zero_three_shotdiet = float(safe_get(columns[10]) or 0)
+    three_ten_shotdiet = float(safe_get(columns[11]) or 0)
+    ten_sixteen_shotdiet = float(safe_get(columns[12]) or 0)
+    sixteen_threep_shotdiet = float(safe_get(columns[13]) or 0)
+    threep_shotdiet = float(safe_get(columns[14]) or 0)
+    twop_p = float(safe_get(columns[15]) or 0)
+    zero_three_p = float(safe_get(columns[16]) or 0)
+    three_ten_p = float(safe_get(columns[17]) or 0)
+    ten_sixteen_p = float(safe_get(columns[18]) or 0)
+    sixteen_threep_p = float(safe_get(columns[19]) or 0)
+    threep_p = float(safe_get(columns[20]) or 0)
+    twop_assisted_p = float(safe_get(columns[21]) or 0)
+    threep_assisted_p = float(safe_get(columns[22]) or 0)
+    dunk_shotdiet = float(safe_get(columns[23]) or 0)
+    dunks = int(safe_get(columns[24]) or 0)
+    cornerthreep_diet = float(safe_get(columns[25]) or 0)
+    cornerthreep_p = float(safe_get(columns[26]) or 0)
+    heave_attempt = int(safe_get(columns[27]) or 0)
+    heave = int(safe_get(columns[28]) or 0)
+    cur.execute(
+    "INSERT INTO shooting_2024 (pid, name, age, team, pos, games_played, games_started, minutes, fgp, avg_fg_dist, twop_shotdiet, zero_three_shotdiet, three_ten_shotdiet, ten_sixteen_shotdiet, sixteen_threep_shotdiet, threep_shotdiet, twop_p, zero_three_p, three_ten_p, ten_sixteen_p, sixteen_threep_p, threep_p, twop_assisted_p, threep_assisted_p, dunk_shotdiet, dunks, cornerthreep_diet, cornerthreep_p, heave_attempt, heave) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
+    (pid, name, age, team, pos, g, gs, mp, fgp, avg_fg_dist, twop_shotdiet, zero_three_shotdiet, three_ten_shotdiet, ten_sixteen_shotdiet, sixteen_threep_shotdiet, threep_shotdiet, twop_p, zero_three_p, three_ten_p, ten_sixteen_p, sixteen_threep_p, threep_p, twop_assisted_p, threep_assisted_p, dunk_shotdiet, dunks, cornerthreep_diet, cornerthreep_p, heave_attempt, heave)
+    )
+    con.commit()
+    cur.close()
+    con.close()
+
+def add_to_adj_shooting(columns):
+    con = None
+    con = connect_to_db(con)
+    con.commit()
+    cur = con.cursor()
+    raw_name = safe_get(columns[0])
+    name = standardize_name(raw_name)
+    pid = generate_pid(name)
+    age = int(safe_get(columns[1]) or 0)
+    team = safe_get(columns[2])
+    pos = safe_get(columns[3])
+    g = int(safe_get(columns[4]) or 0) 
+    gs = int(safe_get(columns[5]) or 0)  
+    mp = float(safe_get(columns[6]) or 0) 
+    fgp = float(safe_get(columns[7]) or 0)
+    twopp = float(safe_get(columns[8]) or 0)
+    threepp = float(safe_get(columns[9]) or 0)
+    efg= float(safe_get(columns[10]) or 0)
+    ftp = float(safe_get(columns[11]) or 0)
+    ts = float(safe_get(columns[12]) or 0)
+    ftr = float(safe_get(columns[13]) or 0)
+    threepar = float(safe_get(columns[14]) or 0)
+    fg_adj = int(safe_get(columns[15]) or 0)
+    twop_adj = int(safe_get(columns[16]) or 0)
+    threep_adj = int(safe_get(columns[17]) or 0)
+    efg_adj = int(safe_get(columns[18]) or 0)
+    ft_adj = int(safe_get(columns[19]) or 0)
+    ts_adj = int(safe_get(columns[20]) or 0)
+    ftr_adj = int(safe_get(columns[21]) or 0)
+    threepar_adj = int(safe_get(columns[22]) or 0)
+    fg_added = float(safe_get(columns[23]) or 0)
+    ts_added = float(safe_get(columns[24]) or 0)
+    cur.execute(
+    "INSERT INTO adj_shooting_2024 (pid, name, age, team, pos, games_played, games_started, minutes, fgp, twopp, threepp, efg, ftp, ts, ftr, threepar, fg_adj, twop_adj, threep_adj, efg_adj, ft_adj, ts_adj, ftr_adj, threepar_adj, fg_added, ts_added) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
+    (pid, name, age, team, pos, g, gs, mp, fgp, twopp, threepp, efg, ftp, ts, ftr, threepar, fg_adj, twop_adj, threep_adj, efg_adj, ft_adj, ts_adj, ftr_adj, threepar_adj, fg_added, ts_added)
     )
     con.commit()
     cur.close()
--- backend/app/scrape/scraper.py
+++ backend/app/scrape/scraper.py
@@ -1,6 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-from api_helpers import add_to_db, create_db
+from api_helpers import add_to_pergame, add_to_per36, add_to_per100, add_to_advanced, add_to_pbp, add_to_shooting, add_to_adj_shooting, create_db
 
 def is_dupe(prev, curr):
     if (prev == curr):
@@ -10,29 +10,32 @@ def is_dupe(prev, curr):
     
 def determine_table(name, columns):
     if (name == "Per Game"):
-        add_to_db(columns)
-        return ""
+        add_to_pergame(columns)
+        return
     elif (name == "Per 36 Min"):
-        # add_to_per36_table(columns)
-        return ""
+        add_to_per36(columns)
+        return
     elif (name == "Per 100 Poss"):
-        # add_to_per100_table(columns)
-        return ""
+        add_to_per100(columns)
+        return
     elif (name == "Advanced"):
-        # add_to_advanced_table(columns)
-        return ""
+        add_to_advanced(columns)
+        return
     elif (name == "Play-by-Play"):
-        # add_to_pbp_table(columns)
-        return ""
+        add_to_pbp(columns)
+        return
     elif (name == "Shooting"):
-        # add_to_shooting_table(columns)
-        return ""
+        add_to_shooting(columns)
+        return
     elif (name == "Adjusted Shooting"):
-        # add_to_adjshooting_table(columns)
-        return ""
-    return ""
+        add_to_adj_shooting(columns)
+        return
+    else:
+        return
 
-def scrape_page(content):
+def scrape_page(table_name, result_url):
+    response = requests.get(result_url)
+    content = response.text
     soup = BeautifulSoup(content, 'lxml')
     table = soup.find('table', class_='stats_table')
     dupe_detector = ""
@@ -46,40 +49,32 @@ def scrape_page(content):
             if (name ==  lgAvg):
                 continue
             dupe_detector = name
-            add_to_db(columns)
-    return ""
+            determine_table(table_name, columns)
+    return
 
-def scrape_nba_data():
+def scrape_year_data():
+    # Establish the root website (Basketball Reference)
     root = "https://www.basketball-reference.com"
     website = f'{root}/leagues/NBA_2024_per_game.html'
 
+    # Make a request to the website to get the page content
     response = requests.get(website)
     content = response.text
     soup = BeautifulSoup(content, 'lxml')
-
-    sections = soup.find_all('div', class_="filter")
-    for link in sections[0].find_all('a', href=True):
-        print("Href: ", link)
-        result = requests.get(f'{root}/{link}')
-        content = result.text
-        # scrape_page(content)
-        # soup = BeautifulSoup(content, 'lxml')
     
-    table = soup.find('table', class_='stats_table')
-    dupe_detector = ""
     create_db()
-    for row in table.tbody.find_all('tr'):
-        columns = row.find_all('td')
-        if (columns != []):
-            name = columns[0].text.strip()
-            lgAvg = "League Average"
-            if (name == dupe_detector): 
-                continue 
-            if (name ==  lgAvg):
-                continue
-            dupe_detector = name
-            add_to_db(columns)
+    # Find all the sections on the page (ex. Per Game, Per 36, Per 100 Possessions)
+    sections = soup.find_all('div', class_="filter")
+
+    # Scrape through each of these tables
+    for link in sections[0].find_all('a', href=True):
+        table_name = link.text.strip()
+        href = link['href'] 
+        result_url = f'{root}{href}' 
+        print("Table Name: ", table_name)
+        print("Resulting URL: ", result_url)
+        scrape_page(table_name, result_url)
     return ""
 
 if __name__ == "__main__":
-    scrape_nba_data()
+    scrape_year_data()
