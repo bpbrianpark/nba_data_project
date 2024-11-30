1e01b2e04eb09c62215d0d250b7da98768a3d402
Brian Park <bmp1208@students.cs.ubc.ca>
2024-11-30 15:52:48 -0800

    added filter to main page only

--- frontend/src/pages/MainPage.js
+++ frontend/src/pages/MainPage.js
@@ -24,9 +24,21 @@ const MainPage = () => {
         2023: [
             { value: 'pergame_2023', label: 'Per Game' },
             { value: 'per36_2023', label: 'Per 36' },
+            { value: 'per100_2023', label: 'Per 100 Possessions' },
             { value: 'advanced_2023', label: 'Advanced' },
+            { value: 'pbp_2023', label: 'Play-by-Play' },
             { value: 'shooting_2023', label: 'Shooting' },
+            { value: 'adj_shooting_2023', label: 'Adjusted Shooting' },
         ],
+        2022: [
+            { value: 'pergame_2022', label: 'Per Game' },
+            { value: 'per36_2022', label: 'Per 36' },
+            { value: 'per100_2022', label: 'Per 100 Possessions' },
+            { value: 'advanced_2022', label: 'Advanced' },
+            { value: 'pbp_2022', label: 'Play-by-Play' },
+            { value: 'shooting_2022', label: 'Shooting' },
+            { value: 'adj_shooting_2022', label: 'Adjusted Shooting' },
+        ]
     };
 
     const fetchTableData = async () => {
