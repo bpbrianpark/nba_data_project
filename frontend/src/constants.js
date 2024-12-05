1536a251484e8192382d5ca953b0757162e4163a
Brian Park <bmp1208@students.cs.ubc.ca>
2024-12-05 02:53:46 -0800

    moved constants

---
+++ frontend/src/constants.js
@@ -0,0 +1,31 @@
+export const allTables = {
+    2024: [
+        { value: 'pergame_2024', label: 'Per Game' },
+        { value: 'per36_2024', label: 'Per 36' },
+        { value: 'per100_2024', label: 'Per 100 Possessions' },
+        { value: 'advanced_2024', label: 'Advanced' },
+        { value: 'pbp_2024', label: 'Play-by-Play' },
+        { value: 'shooting_2024', label: 'Shooting' },
+        { value: 'adj_shooting_2024', label: 'Adjusted Shooting' },
+    ],
+    2023: [
+        { value: 'pergame_2023', label: 'Per Game' },
+        { value: 'per36_2023', label: 'Per 36' },
+        { value: 'per100_2023', label: 'Per 100 Possessions' },
+        { value: 'advanced_2023', label: 'Advanced' },
+        { value: 'pbp_2023', label: 'Play-by-Play' },
+        { value: 'shooting_2023', label: 'Shooting' },
+        { value: 'adj_shooting_2023', label: 'Adjusted Shooting' },
+    ],
+    2022: [
+        { value: 'pergame_2022', label: 'Per Game' },
+        { value: 'per36_2022', label: 'Per 36' },
+        { value: 'per100_2022', label: 'Per 100 Possessions' },
+        { value: 'advanced_2022', label: 'Advanced' },
+        { value: 'pbp_2022', label: 'Play-by-Play' },
+        { value: 'shooting_2022', label: 'Shooting' },
+        { value: 'adj_shooting_2022', label: 'Adjusted Shooting' },
+    ]
+};
+
+export const allPositions = ['PG', 'SG', 'SF', 'PF', 'C'];
